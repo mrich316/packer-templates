5c4e114ba0e44570be3e46f3735788fcfe9b00b5
mrich316 <mrich316@gmail.com>
2017-03-05 08:16:14 -0500

    fix: install vbox-cert from guest additions cdrom.
    - fixes failing build using virtualbox-5.1.14 when installing guest addititions from cdrom.

--- windows/scripts/virtualbox-guest-additions.bat
+++ windows/scripts/virtualbox-guest-additions.bat
@@ -3,5 +3,12 @@
 :: There needs to be Oracle CA (Certificate Authority) certificates installed in order
 :: to prevent user intervention popups which will undermine a silent installation.
 cmd /c certutil -addstore -f "TrustedPublisher" A:\oracle-cert.cer
-cmd /c E:\VBoxWindowsAdditions.exe /S
 
+:: Install cert from iso if found.
+if exist E:\cert\vbox-sha256-r3.cer (
+    cmd /c certutil -addstore -f "TrustedPublisher" E:\cert\vbox-sha256-r3.cer
+)
+
+echo Installing VBoxWindowsAdditions...
+cmd /c E:\VBoxWindowsAdditions.exe /S
+echo Done VBoxWindowsAdditions.
